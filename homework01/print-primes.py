dca8affc0bcb56d3fd5a2342220b0df8032481ad
lukewilson <lukewilson@utexas.edu>
2022-01-31 17:25:05 -0600

    updated loop to only check 1-sqrt(x)

--- homework01/print-primes.py
+++ homework01/print-primes.py
@@ -1,11 +1,16 @@
+# Author: Luke Wilson
+# Last Updated: 1/31/2022
+
 import math
+
 
 for i in range(3,101):
 	is_prime = True
-	for j in range(2,i):
+	for j in range(2,math.floor(math.sqrt(i))):
+		# loop through every number below i to see if its a factor
 		if( i % j == 0):
 			is_prime = False
-			exit
+			exit		# exits loop when factor is found
 	if(is_prime == True):
 		print(i)
 
