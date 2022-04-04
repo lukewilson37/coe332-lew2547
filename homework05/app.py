94d696a24ccf6f905991c7ae3670e57947228048
lukewilson <lukewilson@utexas.edu>
2022-04-04 15:19:15 -0500

    added function definitions to app.py

--- homework05/app.py
+++ homework05/app.py
@@ -7,6 +7,15 @@ import requests
 app = Flask(__name__)
 
 def get_redis_client():
+	"""
+	Returns redis client object for use in flask app. Hard coded for TACC isp02 port 6379 and lew2547's unique host
+
+	Args:
+		none
+
+	Returns:
+		redis client object
+	"""
 	return redis.Redis(host='172.17.0.6', port=6379)
 
 @app.route('/',methods=['GET'])
@@ -15,6 +24,18 @@ def hello_world():
 
 @app.route('/data',methods=['GET','POST'])
 def data_route():
+	"""
+	The route has two functions. 
+	For a POST request. The function updates the redis database thorugh a python redis server object. 
+	For a GET request. The function returns the meteorite landind data loaded from the POST request.
+
+	Args:
+		none
+	Returns
+		if POST: confirmations string
+		if GET: json of meteorite landing data 
+
+	"""
 	if request.method == 'POST':
 		rd = get_redis_client()
 		ml_raw = requests.get('https://raw.githubusercontent.com/wjallen/coe332-sample-data/main/ML_Data_Sample.json')
